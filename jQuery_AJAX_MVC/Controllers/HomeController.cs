9ec2e3f2b129a277e63b92458429a25b6e811d74
vuittonatehsn@gmail.com <A1234568>
2016-11-29 18:50:11 +0800

    加入專案檔案。

---
+++ jQuery_AJAX_MVC/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using jQuery_AJAX_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQuery_AJAX_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AjaxMethod(string name)
+        {
+            PersonModel person = new PersonModel
+            {
+                Name = name,
+                DateTime = DateTime.Now.ToString()
+            };
+            return Json(person);
+        }
+
+        //Demo 1------------
+        [HttpPost]
+        public JsonResult AjaxMethodModel(PersonModel person)
+        {
+            
+            return Json(person);
+        }
+
+        //Demo 2
+        public ActionResult Item()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AjaxItem(Item person)
+        {
+
+            return Json(person);
+        }
+
+
+        //Demo 3
+        [HttpPost]
+        public JsonResult AjaxListItem(List<Item> personList)
+        {
+
+            return Json(personList);
+        }
+
+
+    }
+}
---
+++ jQuery_AJAX_MVC/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jQuery_AJAX_MVC.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public List<string> lastName { get; set; }
+
+    }
+}
